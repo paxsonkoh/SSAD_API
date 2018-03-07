931a2a48805ea9759964da7625719ef8fad1a58d
DESKTOP-QRR9G90\paxson <paxsonkoh300@gmail.com>
2018-03-08 04:44:02 +0800

    ssad api

---
+++ WebApplication4/Controllers/Report911Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication4.Models;
+
+    namespace WebApplication4.Controllers
+    {
+        public class Report911Controller : ApiController
+        {
+        DBReport911 DBReport911 = new DBReport911();
+        public object Get()
+        {
+            var Report911s = from d in DBReport911.Report911s select d;
+            return Request.CreateResponse(HttpStatusCode.OK, Report911s);
+        }
+        public object Post([FromBody]Report911 value)
+            {
+           
+            DBReport911.Report911s.Add(value);
+            DBReport911.SaveChanges();
+            return Request.CreateResponse(HttpStatusCode.OK,value);
+            }
+        }
+    }
---
+++ WebApplication4/Models/Report911.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace WebApplication4.Models
+{
+    public class TimeOfEmergency
+    {
+        public int TimeOfEmergencyID { get; set; }
+        public long Ticks { get; set; }
+        public int Days { get; set; }
+        public int Hours { get; set; }
+        public int Milliseconds { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+        public double TotalDays { get; set; }
+        public int TotalHours { get; set; }
+        public int TotalMilliseconds { get; set; }
+        public int TotalMinutes { get; set; }
+        public int TotalSeconds { get; set; }
+    }
+
+    public class IncidentCreatedTime
+    {
+        public int IncidentCreatedTimeID { get; set; }
+        public long Ticks { get; set; }
+        public int Days { get; set; }
+        public int Hours { get; set; }
+        public int Milliseconds { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+        public double TotalDays { get; set; }
+        public double TotalHours { get; set; }
+        public int TotalMilliseconds { get; set; }
+        public double TotalMinutes { get; set; }
+        public int TotalSeconds { get; set; }
+    }
+
+    public class Report911
+    {
+        public int Report911ID { get; set; }
+        public string CallerName { get; set; }
+        public string CallerNRIC { get; set; }
+        public int CallBackNo { get; set; }
+        public string LocationofEmergency { get; set; }
+        public TimeOfEmergency TimeOfEmergency { get; set; }
+        public string TypeOfSuperBeing { get; set; }
+        public int EstimatedNoOfBeings { get; set; }
+        public int EstimatedNoOfInjuries { get; set; }
+        public int EstimatedNoOfDeath { get; set; }
+        public string DescOfEmergency { get; set; }
+        public string StaffId { get; set; }
+        public string AlertedCMOId { get; set; }
+        public IncidentCreatedTime IncidentCreatedTime { get; set; }
+        public DateTime IncidentCreatedDate { get; set; }
+        public string NatureOfEmergency { get; set; }
+        public string NameOfEmergency { get; set; }
+    }
+    public class DBReport911 : DbContext
+    {
+        public DbSet<Report911> Report911s { get; set; }
+        public DbSet<IncidentCreatedTime> IncidentCreatedTimes { get; set; }
+        public DbSet<TimeOfEmergency> TimeOfEmergencys { get; set; }
+    }
+}
